cd93e8f31828c8cfbf8eba334becc63d0c114cd2
SamuelHaerin <samuel11bahls@gmail.com>
2025-05-17 19:15:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerText = "Tire a preocupação da sua cabeça, quanto mais escura a noite, mais brilhantes são as estrelas✨";
+}
